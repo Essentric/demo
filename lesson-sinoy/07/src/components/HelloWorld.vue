4afd1d4acc42cf28ebda9f492b0a4eeccfa467cc
Essentric <essentric@foxmail>
2018-07-07 13:35:57 +0800

    lesson7

--- lesson-sinoy/07/src/components/HelloWorld.vue
+++ lesson-sinoy/07/src/components/HelloWorld.vue
@@ -1,49 +1,59 @@
 <template>
-  <div class="car">
-    <h1>购物车</h1>
-    <el-table
-      :data="tableData"
-      tooltip-effect="dark"
-      style="width: 100%">
-      <el-table-column
-        prop="name"
-        label="商品信息">
-      </el-table-column>
-      <el-table-column
-        prop="description"
-        label="商品描述">
-      </el-table-column>
-      <el-table-column
-        prop="price"
-        label="单价"
-        show-overflow-tooltip>
-      </el-table-column>
-      <el-table-column
-        prop="num"
-        label="数量">
-        <template slot-scope="scope">
-          <el-input-number
-          size="mini"
-          v-model="scope.row.num"
-          @change="handleNumChange(scope.row)"
-          :min="0" label="描述文字"></el-input-number>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="total"
-        label="总价 / (元)"
-        show-overflow-tooltip>
-      </el-table-column>
-    </el-table>
-    <el-row>
-      <el-col :span="12">
-        合计: {{getTotal}}
-      </el-col>
-      <el-col :span="6">
-        <el-button type="primary">结算</el-button>
-      </el-col>
-    </el-row>
-  </div>
+  <el-container>
+    <el-header>
+      <h3>我的购物车({{goodsData.length}})</h3>
+      <a href="javascript:;" class="btn-edit">编辑</a>
+    </el-header>
+    <el-main>
+      <el-row class="goods" v-for="(goods, key) in goodsData" :key="key">
+        <el-col :span="24" class="store-name">
+          <el-row>
+            <el-col :span="12">
+              <el-checkbox-group v-model="goods.goodsList">
+                <el-checkbox></el-checkbox>
+              </el-checkbox-group>
+              <a href="javascript:;">{{goods.storeName}} ></a>
+            </el-col>
+            <el-col :span="6" :offset="6" style="text-align:right;padding-right:1em">
+              <a href="javascript:;" class="get-coupon">领券</a>
+              <i style="color: #eee;">|</i>
+              <a href="javascript:;">编辑</a>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="24" class="goods-info">
+          <el-col :span="8" class="pic">
+            <el-checkbox-group v-model="goods.goodsList" class="checkbox">
+              <el-checkbox></el-checkbox>
+            </el-checkbox-group>
+            <a href="javascript:;" style="margin-left:2em;width:100%;height:100%"><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1530945037524&di=914d7711636e8c7fb7cf8702372b02d6&imgtype=0&src=http%3A%2F%2Fimg14.360buyimg.com%2Fn4%2Fg7%2FM03%2F05%2F12%2FrBEHZlBJyNYIAAAAAAFJgkuWxI0AABFkgG4mrMAAUma420.jpg" alt=""></a>
+          </el-col>
+          <el-col :span="16" class="desc" style="padding-right:1em">
+            <a href="javascript:;">
+              <p>{{goods.description}}</p>
+              <p>
+                <span style="color: #f00">￥ {{goods.price}}</span>
+                <span style="float: right">x {{goods.count}}</span>
+              </p>
+            </a>
+          </el-col>
+        </el-col>
+      </el-row>
+      <div class="my-submit">
+        <el-row class="submit">
+          <el-col :span="4">
+            <el-checkbox-group>
+              <el-checkbox>全选</el-checkbox>
+            </el-checkbox-group>
+          </el-col>
+          <el-col :span="20" style="text-align:right">
+            <span>合计: <span style="color: #f00">￥998</span></span>
+            <a href="javascript:;" style="height:100%;margin-left: 1em;padding: 1em 2em;border:1px solid #f00; background: orange; color:#fff">结算</a>
+          </el-col>
+        </el-row>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -51,97 +61,107 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      tableData: [{
-        id: 5,
-        num: 1,
-        name: 'iPhoneX',
-        description: '苹果十代',
-        price: 10000,
-        total: 0
+      goodsData: [{
+        storeName: '杂七杂八批发生活',
+        description: '双肩包男士背包男韩版潮旅行电脑男潮包PU时尚潮流皮休闲学生书包',
+        price: 99.8,
+        count: 1,
+        goodsList: []
       }, {
-        id: 9,
-        num: 1,
-        name: 'Samsung9',
-        description: '三星九代',
-        price: 9000,
-        total: 0
+        storeName: '杂七杂八批发生活',
+        description: '双肩包男士背包男韩版潮旅行电脑男潮包PU时尚潮流皮休闲学生书包',
+        price: 99.8,
+        count: 1,
+        goodsList: []
       }, {
-        id: 4,
-        num: 1,
-        name: '小米8',
-        description: '小米八代',
-        price: 8000,
-        total: 0
+        storeName: '杂七杂八批发生活',
+        description: '双肩包男士背包男韩版潮旅行电脑男潮包PU时尚潮流皮休闲学生书包',
+        price: 99.8,
+        count: 1,
+        goodsList: []
       }, {
-        id: 16,
-        num: 1,
-        name: '华为7',
-        description: '华为七代',
-        price: 7000,
-        total: 0
-      }, {
-        id: 10,
-        num: 1,
-        name: '一加6',
-        description: '一加六代',
-        price: 6000,
-        total: 0
-      }],
-      multipleTable: []
+        storeName: '杂七杂八批发生活',
+        description: '双肩包男士背包男韩版潮旅行电脑男潮包PU时尚潮流皮休闲学生书包',
+        price: 99.8,
+        count: 1,
+        goodsList: []
+      }]
     }
   },
-  computed: {
-    getTotal () {
-      let total = 0
-      this.tableData.forEach((ele) => {
-        ele.total = ele.num * ele.price
-      })
-      total = 0
-      this.tableData.forEach((element) => {
-        total = total + element.total
-      })
-      return total
-    }
-  },
-  methods: {
-    handleNumChange (row) {
-      if (row.num === 0) {
-        this.$confirm('移出购物车？', {
-          confrimButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.$message({
-            type: 'success',
-            message: '已成功移除!'
-          })
-          this.tableData.forEach((ele, index) => {
-            if (ele.id === row.id) {
-              this.tableData.splice(index, 1)
-            }
-          })
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消移除!'
-          })
-          row.num = 1
-        })
-      }
-    }
-  }
+  computed: {},
+  methods: {}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-  .cell {
-    text-align: center;
-  }
-  .el-row {
-    margin-top: 30px;
-  }
-  .el-col {
-    line-height: 40px;
-  }
+<style scoped>
+a {
+  text-decoration: none;
+  color: #000;
+}
+p {
+  padding: 0;
+  margin: 0;
+}
+.el-container {
+  width: 100%;
+  padding: 0;
+}
+.el-header {
+  position: relative;
+  padding: 0;
+  width: 100%;
+  background: #281B35;
+}
+.el-main {
+  background: #FAFAFA;
+  padding: 0;
+}
+.goods {
+  background: #fff;
+  margin: .5em 0;
+}
+.store-name {
+  padding: 1em 0;
+  border-bottom: 1px solid #eee;
+}
+.goods-info {
+  position: relative;
+}
+.checkbox {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+.desc {
+  padding: .5em 1em;
+}
+.el-checkbox-group {
+  display: inline-block;
+  text-align: center;
+}
+.el-checkbox {
+  width: 1.5em;
+}
+h3 {
+  text-align: center;
+  color: #fff;
+}
+.btn-edit {
+  color: #fff;
+  position: absolute;
+  top: 50%;
+  right: 1em;
+  transform: translateY(-50%);
+}
+.my-submit {
+  position: fixed;
+  bottom: 0;
+  border-top: 1px solid #eee;
+  width: 100%;
+  background: #fff;
+}
+.submit {
+  padding: 1em 0;
+}
 </style>
